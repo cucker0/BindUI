4a56c135abb5f4bb8f25f48556e0b236a05eeaad
cucker <hanxiao2100@qq.com>
2020-01-21 15:49:05 +0800

    优化 static/bootstrap/js/autoPostCsrftoken.js

--- static/bootstrap/js/autoPostCsrftoken.js
+++ static/bootstrap/js/autoPostCsrftoken.js
@@ -1,12 +1,19 @@
-(function(jq){
-    /***
-     * 专门用于 AJAX 自动处理csrf
-     * @param 自动从cookies中获取 csrftoken并自动添把获取的 csrftoken添加到RequestHeader中
-     * @returns {*}
-     */
+/**
+* 专门用于 AJAX post提交时自动处理csrftoken
+*
+* 自动从cookies中获取 csrftoken并自动添把获取的 csrftoken添加到RequestHeader中
+*/
+(function (jq) {
 
-    function getCookie(name){
-        //从cookie中获取名为 csrftoken 的值并返回，默认django会response一个csrftoken值为csrf_token的cookie
+    /**
+     * 从cookie中获取名为 csrftoken 的值并返回
+     *
+     * 默认django会response一个csrftoken值为csrf_token的cookie
+     *
+     * @param name: cookie名
+     * @returns: 获取的cookie值
+     */
+    function getCookie(name) {
         var cookieValue = null;
         if (document.cookie && document.cookie != '') {
             var cookies = document.cookie.split(';');
@@ -23,7 +30,6 @@
     }
 
     var csrftoken = getCookie('csrftoken');
-
     //The above code could be simplified by using the JavaScript Cookie library to replace getCookie:　
     // need include js.cookie.js
     //var csrftoken = Cookies.get('csrftoken');
@@ -33,14 +39,16 @@
         // these HTTP methods do not require CSRF protection
         return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
     }
+
+    // 设置ajax发送前执行的方法，这是全局生效的
     $.ajaxSetup({
-        beforeSend: function(xhr, settings) {
-            if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+        beforeSend: function (xhr, settings) {
+            if (!csrfSafeMethod(settings.type) && !this.crossDomain) { // !this.crossDomain 表示不跨域
                 xhr.setRequestHeader("X-CSRFToken", csrftoken);
                 //console.log(csrftoken);
             }
         }
     });
 
-})(jQuery)
+})(jQuery);
 
